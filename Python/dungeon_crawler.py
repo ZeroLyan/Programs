c652af0a477369f73d1896edd4c61df0b5284ce6
JGeorge9 <82701583+ZeroLyan@users.noreply.github.com>
2026-03-18 20:55:57 -0700

    Implement player defense option in combat
    Added player defending logic to combat mechanics.

--- Python/dungeon_crawler.py
+++ Python/dungeon_crawler.py
@@ -34,9 +34,13 @@ def fight(player, enemy):
         
     print(f"{player.name} encountered a {enemy.name}. Fight!\n")
     print(f"{initiative_order[0][1]} is first\n")
-    
+    player_defending = False
+    enemy_defendings = False
+    time.sleep(1)
+
     while True:
 
+         
         if enemy.SPD <= player.SPD:
             print(f"{player.name} HP: {player.HP}     {enemy.name} HP: {enemy.HP}\n")
             print("*" * 28)
@@ -49,9 +53,16 @@ def fight(player, enemy):
         else:
             
             print(f"{enemy.name} chooses to attack {player.name}\n")
-            player.HP -= enemy.ATK
-            print(f"{enemy.name} dealt {enemy.ATK} damage to {player.name}\n")
-           
+            if player_defending == True:
+
+                player.HP -= enemy.ATK - player.DEF
+                print(f"{enemy.name} dealt {enemy.ATK- player.DEF} damage to {player.name}\n")
+            
+            else:
+            
+                player.HP -= enemy.ATK
+                print(f"{enemy.name} dealt {enemy.ATK} damage to {player.name}\n")
+
             print(f"{player.name} HP: {player.HP}     {enemy.name} HP: {enemy.HP}\n")
             
             print("*" * 28)
@@ -64,6 +75,10 @@ def fight(player, enemy):
                 print(f"\n{player.name} attacks {enemy.name} for {player.ATK} damage\n")
                 enemy.HP -= player.ATK
                 time.sleep(1)
+            if player_choice == 2:
+                player_defending = True
+                print(f"\n{player.name} is defending.\n")
+                
             elif player_choice == 4:
                 break
 
